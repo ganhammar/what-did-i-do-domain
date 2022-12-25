d969bf2ff459e0c85b3ccde55bcde319e859c78c
Anton Ganhammar <ganhammar@gmail.com>
2022-12-25 18:03:57 +0100

    Fix: Test, remove Shared dependency

--- src/App.Api/CreateEvent/Startup.cs
+++ src/App.Api/CreateEvent/Startup.cs
@@ -1,37 +1,37 @@
-using System.Reflection;
-using Amazon.DynamoDBv2;
-using Amazon.XRay.Recorder.Core;
-using Amazon.XRay.Recorder.Handlers.AwsSdk;
-using App.Api.Shared.Infrastructure;
-using MediatR;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+// using System.Reflection;
+// using Amazon.DynamoDBv2;
+// using Amazon.XRay.Recorder.Core;
+// using Amazon.XRay.Recorder.Handlers.AwsSdk;
+// using App.Api.Shared.Infrastructure;
+// using MediatR;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
 
-namespace App.Api.CreateEvent;
+// namespace App.Api.CreateEvent;
 
-public class Startup
-{
-    public static IConfiguration Configuration = FunctionConfiguration.Get();
+// public class Startup
+// {
+//     public static IConfiguration Configuration = FunctionConfiguration.Get();
 
-    public static IServiceCollection ConfigureServices()
-    {
-        var services = new ServiceCollection();
+//     public static IServiceCollection ConfigureServices()
+//     {
+//         var services = new ServiceCollection();
 
-        AWSSDKHandler.RegisterXRayForAllServices();
-#if DEBUG
-        AWSXRayRecorder.Instance.XRayOptions.IsXRayTracingDisabled = true;
-#endif
+//         AWSSDKHandler.RegisterXRayForAllServices();
+// #if DEBUG
+//         AWSXRayRecorder.Instance.XRayOptions.IsXRayTracingDisabled = true;
+// #endif
 
-        var dynamoDbConfig = Configuration.GetSection("DynamoDB");
+//         var dynamoDbConfig = Configuration.GetSection("DynamoDB");
 
-        services
-            .AddDefaultAWSOptions(Configuration.GetAWSOptions())
-            .AddSingleton<IAmazonDynamoDB>(_ => new AmazonDynamoDBClient(new AmazonDynamoDBConfig
-            {
-                ServiceURL = dynamoDbConfig["ServiceUrl"],
-            }));
-        services.AddMediatR(Assembly.GetCallingAssembly());
+//         services
+//             .AddDefaultAWSOptions(Configuration.GetAWSOptions())
+//             .AddSingleton<IAmazonDynamoDB>(_ => new AmazonDynamoDBClient(new AmazonDynamoDBConfig
+//             {
+//                 ServiceURL = dynamoDbConfig["ServiceUrl"],
+//             }));
+//         services.AddMediatR(Assembly.GetCallingAssembly());
 
-        return services;
-    }
-}
+//         return services;
+//     }
+// }
