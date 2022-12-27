e9f77a79ee5e65492a06a78aaf84ac1ebdfd731e
Anton Ganhammar <ganhammar@gmail.com>
2022-12-27 10:03:07 +0100

    Feat: Switch to CDK

---
+++ src/App.Stack/AppStack.cs
@@ -0,0 +1,55 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.APIGateway;
+using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.Lambda;
+using Constructs;
+
+namespace AppStack;
+
+public class AppStack : Stack
+{
+    internal AppStack(Construct scope, string id, IStackProps props)
+        : base(scope, id, props)
+    {
+        var applicationTable = new Table(this, "ApplicationTable", new TableProps
+        {
+            TableName = "what-did-i-do",
+            RemovalPolicy = RemovalPolicy.DESTROY, //Delete DynamoDB table on CDK destroy
+            PartitionKey = new Amazon.CDK.AWS.DynamoDB.Attribute
+            {
+                Name = "PartitionKey",
+                Type = AttributeType.STRING,
+            },
+            SortKey = new Amazon.CDK.AWS.DynamoDB.Attribute
+            {
+                Name = "SortKey",
+                Type = AttributeType.STRING,
+            },
+            PointInTimeRecovery = true,
+            BillingMode = BillingMode.PAY_PER_REQUEST,
+        });
+
+        var createEventFunction = new Function(this, "CreateEventFunction", new FunctionProps
+        {
+            Runtime = Runtime.DOTNET_6,
+            Handler = "CreateEvent::App.Api.CreateEvent.Function::FunctionHandler",
+            Code = Code.FromAsset("./.output/CreateEvent.zip"),
+            Timeout = Duration.Minutes(1),
+            MemorySize = 128,
+        });
+        applicationTable.GrantWriteData(createEventFunction);
+
+        var apiGateway = new RestApi(this, "what-did-i-do", new RestApiProps
+        {
+            RestApiName = "what-did-i-do",
+        });
+
+        var eventResource = apiGateway.Root.AddResource("event");
+        eventResource.AddMethod("POST", new LambdaIntegration(createEventFunction));
+
+        new CfnOutput(this, "APIGWEndpoint", new CfnOutputProps
+        {
+            Value = apiGateway.Url,
+        });
+    }
+}
---
+++ src/App.Stack/Program.cs
@@ -0,0 +1,15 @@
+using Amazon.CDK;
+
+namespace AppStack;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var app = new App(null);
+
+        new AppStack(app, "what-did-i-do-stack", new StackProps());
+
+        app.Synth();
+    }
+}
