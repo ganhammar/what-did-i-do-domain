1fde275dd8e56c6d4d758c8a00c09bac70e4787f
Anton Ganhammar <ganhammar@gmail.com>
2022-12-29 21:45:03 +0100

    Fix: Add bad request test to create event

--- test/App.Api/CreateEventTests/FunctionTests.cs
+++ test/App.Api/CreateEventTests/FunctionTests.cs
@@ -4,6 +4,7 @@ using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.TestUtilities;
 using App.Api.CreateEvent;
 using App.Api.Shared.Models;
+using FluentValidation.Results;
 using TestBase;
 using Xunit;
 
@@ -34,5 +35,27 @@ public class FunctionTests
 
         Assert.NotNull(body);
         Assert.Equal(data.Title, body!.Title);
+    }
+
+    [Fact]
+    public async Task Should_ReturnBadRequest_When_InputIsNotValid()
+    {
+        var function = new Function();
+        var context = new TestLambdaContext();
+        var data = new CreateEventCommand.Command();
+        var request = new APIGatewayProxyRequest
+        {
+            HttpMethod = HttpMethod.Post.Method,
+            Body = JsonSerializer.Serialize(data),
+        };
+        var response = await function.FunctionHandler(request, context);
+
+        Assert.Equal((int)HttpStatusCode.BadRequest, response.StatusCode);
+
+        var errors = JsonSerializer.Deserialize<List<ValidationFailure>>(response.Body);
+
+        Assert.NotNull(errors);
+        Assert.Contains(errors, error => error.PropertyName == nameof(CreateEventCommand.Command.Title)
+                && error.ErrorCode == "NotEmptyValidator");
     }
 }
