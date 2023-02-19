80fe7b7b2025796522b4216b306c286b9e96b459
Anton Ganhammar <ganhammar@gmail.com>
2023-02-19 19:10:18 +0100

    fix: correct casing

--- src/App.Stack/AppStack.cs
+++ src/App.Stack/AppStack.cs
@@ -231,7 +231,7 @@ public class AppStack : Stack
     });
     new BucketDeployment(this, "DeployClient", new BucketDeploymentProps
     {
-      Sources = new[] { Source.Asset("./src/Client/Login/build") },
+      Sources = new[] { Source.Asset("./src/Client/login/build") },
       DestinationBucket = clientBucket,
     });
     var policyStatement = new PolicyStatement(new PolicyStatementProps
