0c9c5d47f4916cb53e90b4e9ab489ca05f636d05
Anton Ganhammar <ganhammar@gmail.com>
2023-02-19 17:13:32 +0100

    fix: add tests to isEmail

---
+++ src/Client/shared/src/utils/__tests__/isEmail.test.ts
@@ -0,0 +1,11 @@
+import { isEmail } from "../isEmail";
+
+describe('isEmail', () => {
+  it('is email', () => {
+    expect(isEmail('hello@wdid.fyi')).toBeTruthy();
+  });
+
+  it('is not email', () => {
+    expect(isEmail('hello wdid.fyi')).toBeFalsy();
+  });
+});
