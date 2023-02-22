e6e90af90721d58784d93b3d5daeed79c55538eb
Anton Ganhammar <ganhammar@gmail.com>
2023-02-22 19:29:56 +0100

    fix: basic loading indicator

--- src/Client/shared/src/components/Loader/Loader.tsx
+++ src/Client/shared/src/components/Loader/Loader.tsx
@@ -1,3 +1,37 @@
+import styled, { keyframes } from 'styled-components';
+
+const LoadingIndicatorWrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-left: -20px;
+  margin-top: -20px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const fade = keyframes`
+  0% {
+    -webkit-transform: scale(0);
+  }
+  100% {
+    -webkit-transform: scale(1.0);
+    opacity: 0;
+  }
+`;
+
+const LoadingIndicator = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  animation: ${fade} 1.0s infinite ease-in-out;
+`;
+
 export function Loader() {
-  return <>Laddar</>;
+  return (
+    <LoadingIndicatorWrapper>
+      <LoadingIndicator />
+    </LoadingIndicatorWrapper>
+  );
 }
