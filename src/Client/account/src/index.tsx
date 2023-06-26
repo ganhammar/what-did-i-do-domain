b8db6a5afd73a07412cab5da642044efb228fff3
Anton Ganhammar <ganhammar@gmail.com>
2023-06-26 22:12:09 +0200

    fix: handle silent-renew in account app

--- src/Client/account/src/index.tsx
+++ src/Client/account/src/index.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { reportWebVitals } from '@wdid/shared';
+import { userManager } from '@wdid/shared/src/components/Auth/userManager';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (window.location.pathname === '/login/silent-renew') {
+  userManager.signinSilentCallback();
+} else {
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+}
