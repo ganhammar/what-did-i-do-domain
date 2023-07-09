ce51917011b9426f0196f209dad7751544c3b141
Anton Ganhammar <ganhammar@gmail.com>
2023-07-09 08:25:09 +0200

    feat: add simple test to verify startup works

---
+++ test/Server/App.Login.Tests/StartupTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+
+namespace App.Login.Tests;
+
+public class StartupTests
+{
+  [Fact]
+  public async Task Should_BeNotFound_When_TryingToGetRootPath()
+  {
+    var host = new HostBuilder()
+      .ConfigureWebHost(webBuilder =>
+      {
+        webBuilder.UseEnvironment("Development");
+        webBuilder.UseStartup<Startup>();
+        webBuilder.UseTestServer();
+      })
+      .Build();
+
+    await host.StartAsync();
+
+    var client = host.GetTestServer().CreateClient();
+
+    var response = await client.GetAsync("/");
+
+    Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+  }
+}
