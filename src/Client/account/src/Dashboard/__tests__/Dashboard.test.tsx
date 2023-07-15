9fceb7d498a08cbb35b7f154a0a069b851b868c3
Anton Ganhammar <ganhammar@gmail.com>
2023-07-15 07:25:39 +0200

    fix: dashboard test

--- src/Client/account/src/Dashboard/__tests__/Dashboard.test.tsx
+++ src/Client/account/src/Dashboard/__tests__/Dashboard.test.tsx
@@ -1,10 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { createRecoilMockWrapper } from 'recoil-mock';
 import { Dashboard } from '../Dashboard';
+import { currentAccountAtom } from '../currentAccountAtom';
 
 test('renders application title', async () => {
-  render(<Dashboard />);
+  const name = 'test-account-name';
+  const { context, wrapper } = createRecoilMockWrapper();
+  context.set(currentAccountAtom, {
+    name,
+    id: name,
+    createDate: new Date().toISOString(),
+  });
 
-  const elements = screen.getAllByText(/Dashboard\\?/i);
+  render(
+    <BrowserRouter>
+      <Dashboard />
+    </BrowserRouter>,
+    { wrapper }
+  );
+
+  const elements = screen.getAllByText(new RegExp(name));
   expect(elements.length).toBeGreaterThan(0);
   expect(elements[0]).toBeInTheDocument();
 });
